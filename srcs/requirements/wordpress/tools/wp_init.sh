42c311f69528ed8968baabe3479c1370a2f3401a
Marius Meier <mmeier@student.hive.fi>
2025-04-01 13:53:52 +0300

    update wp script

--- srcs/requirements/wordpress/tools/wp_init.sh
+++ srcs/requirements/wordpress/tools/wp_init.sh
@@ -6,11 +6,37 @@ until mysqladmin ping -h"$DB_HOST" --silent; do
 	sleep 2
 done
 
+chown -R www:www /var/www/html
+chmod -R 755 /var/www/html
+
 # autofill wp startup fields
-wp core download --path="/var/www/html" --allow-root
-wp config create --path="/var/www/html" --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --skip-check
-wp core install --path="/var/www/html" --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$ADM_WP_NAME --admin_password=$ADM_WP_PASS --admin_email=$ADM_WP_EMAIL --skip-email
-wp user create --path="/var/www/html" --allow-root $WP_USERNAME $WP_USEREMAIL --user_pass=$WP_USERPASS --role='contributor'
+wp core download --path=/var/www/html --allow-root
+wp config create \
+	--allow-root \
+	--dbname=$DB_NAME \
+	--dbuser=$DB_USER \
+	--dbpass=$DB_PASS \
+	--dbhost=$DB_HOST \
+	--path=/var/www/html
+
+wp core install \ 
+	--allow-root \
+	--path=/var/www/html \
+	--skip-email \
+	--url="https://$DOMAIN_NAME" \
+	--title=$WP_TITLE \
+	--admin_user=$ADM_WP_NAME \
+	--admin_password=$ADM_WP_PASS \
+	--admin_email=$ADM_WP_EMAIL \
+
+
+
+wp user create \
+	--allow-root \
+	--path="/var/www/html" \ 
+	$WP_USERNAME $WP_USEREMAIL \
+	--role=author \
+	--user_pass=$WP_USERPASS 
 
 #launch php-fpm
-exec /usr/sbin/php-fpm7.4 -F
+exec /usr/sbin/php-fpm82 -F
